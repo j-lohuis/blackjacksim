76830d08bf984465357f8eb856f8535ff3a168c6
Julian Lohuis <lohuisjulian@gmail.com>
2023-08-30 18:41:25 +0200

    Better user input handling in main

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import re
 from dealer import *
 from deck import *
 from player import *
@@ -15,7 +16,7 @@ def play_alone():
 
 
 def simulate(players, rounds, output_file):
-    """Starts simualtion with chosen strategies and plots the results."""
+    """Starts simulation with chosen strategies and plots the results."""
     deck = Deck(6, 0.75)
     dealer = Dealer(deck, players)
 
@@ -31,60 +32,75 @@ def simulate(players, rounds, output_file):
     plt.legend()
     plt.grid(True)
 
-    plt.savefig(output_file)
+    plt.savefig("plots/" + output_file)
 
 
 def main():
     """Entry point of the application. Asks the user what to do and does it accordingly."""
+    print("Choose:")
+    print("    [0] Play by yourself")
+    print("    [1] Simulate")
+
     while True:
-        print("Choose:")
-        print("    [0] Play by yourself")
-        print("    [1] Simulate")
-
         choice = input("> ").strip()
-
-        if choice == '0':
-            play_alone()
-            break
-        elif choice == '1':
-            print("Choose which strategies to simulate:")
-            print("    [0] Random player")
-            print("    [1] Gambler fallacy player")
-            print("    [2] Basic strategy")
-            print("    [3] Hi-Lo card counter")
-            print("    [4] Hi-Opt I card counter")
-            print("    [5] Hi-Opt II card counter")
-            print("    [6] KO card counter")
-            print("    [7] Omega II card counter")
-            print("    [8] Zen count card counter")
-            print("    [9] 10 card counter")
-            print("  Example: 0 1 3 4 5")
-
-            strategies = input("> ").strip().split()
-            print(strategies)
-            players = []
-            for strat in strategies:
-                match strat:
-                    case "0": players.append(RandomPlayer(0))
-                    case "1": players.append(AveragePlayer(0))
-                    case "2": players.append(Optimal_Player(0))
-                    case "3": players.append(Card_Counter("Hi-Lo", 0, 6, STRAT_HI_LO))
-                    case "4": players.append(Card_Counter("Hi-Opt I", 0, 6, STRAT_HI_OPTI))
-                    case "5": players.append(Card_Counter("Hi-Opt II", 0, 6, STRAT_HI_OPTII))
-                    case "6": players.append(Card_Counter("KO", 0, 6, STRAT_KO))
-                    case "7": players.append(Card_Counter("Omega II", 0, 6, STRAT_OMEGAII))
-                    case "8": players.append(Card_Counter("Zen Count", 0, 6, STRAT_ZEN_COUNT))
-                    case "9": players.append(Card_Counter("10 Count", 0, 6, STRAT_10_COUNT))
-                    case _: print("Invalid choice. Please try again.")
-
-            rounds = int(input("How many rounds?\n> ").strip())
-
-            output_file = input("Output plot to file?\n> ").strip()
-
-            simulate(players, rounds, output_file)
+        if re.match(r"^[01]$", choice):
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice, try again")
+
+    if choice == '0':
+        play_alone()
+    elif choice == '1':
+        print("Choose which strategies to simulate:")
+        print("    [0] Random player")
+        print("    [1] Gambler fallacy player")
+        print("    [2] Basic strategy")
+        print("    [3] Hi-Lo card counter")
+        print("    [4] Hi-Opt I card counter")
+        print("    [5] Hi-Opt II card counter")
+        print("    [6] KO card counter")
+        print("    [7] Omega II card counter")
+        print("    [8] Zen count card counter")
+        print("    [9] 10 card counter")
+        print("  Example: 0 1 3 4 5")
+
+
+        strat_list = ""
+        while True:
+            strat_list = input("> ").strip()
+            if re.match(r"^[0-9]( [0-9])*$", strat_list):
+                break
+            else:
+                print("Invalid choice, try again")
+
+        strategies = strat_list.split()
+        players = []
+        for strat in strategies:
+            match strat:
+                case "0": players.append(RandomPlayer(0))
+                case "1": players.append(AveragePlayer(0))
+                case "2": players.append(Optimal_Player(0))
+                case "3": players.append(Card_Counter("Hi-Lo", 0, 6, STRAT_HI_LO))
+                case "4": players.append(Card_Counter("Hi-Opt I", 0, 6, STRAT_HI_OPTI))
+                case "5": players.append(Card_Counter("Hi-Opt II", 0, 6, STRAT_HI_OPTII))
+                case "6": players.append(Card_Counter("KO", 0, 6, STRAT_KO))
+                case "7": players.append(Card_Counter("Omega II", 0, 6, STRAT_OMEGAII))
+                case "8": players.append(Card_Counter("Zen Count", 0, 6, STRAT_ZEN_COUNT))
+                case "9": players.append(Card_Counter("10 Count", 0, 6, STRAT_10_COUNT))
+                case _: print("Invalid choice. Please try again.")
+
+        rounds = 0
+        while True:
+            rounds_input = input("How many rounds?\n> ").strip()
+            if re.match(r"^[0-9]+$", rounds_input):
+                rounds = int(rounds_input)
+                break
+            else:
+                print("Invalid choice, try again")
+
+        output_file = input("Output plot to file?\n> ").strip()
+
+        simulate(players, rounds, output_file)
 
 
 if __name__ == '__main__':
